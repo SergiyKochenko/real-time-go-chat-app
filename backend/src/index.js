80a7468e8456b8e6b98a7d18cdab3fce4a503809
Sergiy <sergiykochenko@gmail.com>
2025-04-18 00:35:31 +0100

    Profile Page

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,18 +12,18 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use(express.json());
+// Increase the body size limit
+app.use(express.json({ limit: '10mb' })); // Adjust the limit as needed
 app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
-  })
-);
+}));
 
-app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on PORT:', PORT);
